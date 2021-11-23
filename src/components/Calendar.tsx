33b8ff8e57713d7229f44d54d0f043a063b4d620
Conor <cmcnamee4@gmail.com>
2021-11-23 00:32:02 +0000

    setup table

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,8 +1,42 @@
+import React from "react";
+
+import "./Calendar.css";
 
 const Calendar = () => {
 
     return (
-        <div>
+        <div className="schedulerAppContainer">
+            <table className="schedulerApp">
+                <tr className="schedulerAppRow">
+                    <td className="schedulerAppCol">
+                    Monday
+                    </td>
+
+                    <td className="schedulerAppCol">
+                        Tuesday
+                    </td>
+
+                    <td className="schedulerAppCol">
+                        Wednesday
+                    </td>
+
+                    <td className="schedulerAppCol">
+                        Thursday
+                    </td>
+
+                    <td className="schedulerAppCol">
+                        Friday
+                    </td>
+
+                    <td className="schedulerAppCol">
+                        Saturday
+                    </td>
+
+                    <td className="schedulerAppCol">
+                        Sunday
+                    </td>
+                </tr>
+            </table>
         </div>
     );
 }
